f051cb36bbaac1ae6fe3c653e30c0916271bedd2
Jim Breese <studiodjb@gmail.com>
2014-12-08 23:45:32 -0800

    blackjack2

--- blackjack_logic_oop.rb
+++ blackjack_logic_oop.rb
@@ -7,7 +7,7 @@ cards = [["Hearts", "Ace"], ["Hearts", "5"], ["Clubs", "2"], ["Hearts", "6"]]
 cards2 = [["Hearts", "2"], ["Hearts", "6"], ["Clubs", "Ace"], ["Hearts", "Ace"], ["Clubs", "9"]]
 
 #how chris showed in the final solution for last lesson
-def get_total(cards)
+def get_old_total(cards)
   total = 0
   value_string = cards.map { |value| value[1] }
   value_string.each do |string|
@@ -54,9 +54,8 @@ def get_new_total(cards)
 end
 
 def final_total(cards)
-  old = get_total(cards)
+  old = get_old_total(cards)
   updated =  get_new_total(cards)
-  binding.pry
   if old > updated && old < 22
     return old
   elsif updated > old && updated < 22
@@ -67,12 +66,24 @@ def final_total(cards)
     return old
   else 
     return updated
-    
   end    
 end
 
 
-
-
-puts final_total(cards2)
-
+puts ''
+puts "First set of cards are: "
+cards.each { |card| puts "#{card[1]} of #{card[0]}"}
+puts '-----------------'
+puts "Totals for first set of cards:"
+puts "Old method = #{get_old_total(cards)}"
+puts "New method = #{get_new_total(cards)}"
+puts "Final total is #{final_total(cards)}"
+puts ''
+puts ''
+puts "Second set of cards are: "
+cards2.each { |card| puts "#{card[1]} of #{card[0]}"}
+puts '-----------------'
+puts "Totals for second set of cards:"
+puts "Old method = #{get_old_total(cards2)}"
+puts "New method = #{get_new_total(cards2)}"
+puts "Final total is #{final_total(cards2)}"
